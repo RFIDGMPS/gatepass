727ecfc195998b333d0634b1f1d2a96643e5a930
gatepass <kyebejeanu@gmail.com>
2024-07-04 21:57:55 +0800

    usersupdate

--- admin/users.php
+++ admin/users.php
@@ -3,7 +3,7 @@
 <html lang="en">
    <head>
       <meta charset="utf-8">
-      <title>Administrator</title>
+      <title>Admin123</title>
       <meta content="width=device-width, initial-scale=1.0" name="viewport">
       <meta content="" name="keywords">
       <meta content="" name="description">
@@ -57,12 +57,12 @@
    </head>
    <body>
       <div class="container-fluid position-relative bg-white d-flex p-0">
-         <!-- Spinner Start -->
+         <!-- Spinner Start
          <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
                <span class="sr-only">Loading...</span>
             </div>
-         </div>
+         </div> -->
          <!-- Spinner End -->
          <!-- Sidebar Start -->
          <div class="sidebar pe-4 pb-3" style="background-color: #fcaf42">
