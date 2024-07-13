12506537e509ef747cd9d09bb1c09b90bb3e6868
RFIDGMPS <kyebejeanu@gmail.com>
2024-07-13 13:29:21 +0800

    change

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
- $db = mysqli_connect('127.0.0.1', 'root', '1Gatepassdb') or
+ <?php
+ $db = mysqli_connect('127.0.0.1', 'u510162695_gatepassdb', '1Gatepassdb') or
         die ('Unable to connect. Check your connection parameters.');
         mysqli_select_db($db, 'u510162695_gatepassdb' ) or die(mysqli_error($db));
 ?>
