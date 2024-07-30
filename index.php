fe8aaa2710967e215eca63bc662513e3a1bad99b
RFIDGMPS <kyebejeanu@gmail.com>
2024-07-30 14:58:34 +0800

    1

--- index.php
+++ index.php
@@ -6,8 +6,7 @@ $logo1 = "";
     $address = "";
     $logo2 = "";
 // Fetch data from the about table
-//$sql = "SELECT * FROM about LIMIT 1";
-$sql = "truncate table visitor_logs";
+$sql = "SELECT * FROM about LIMIT 1";
 
 $result = $db->query($sql);
 
