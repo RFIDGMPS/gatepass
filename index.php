427251a89c506f3149abb217f00f16f1228c09a1
RFIDGMPS <kyebejeanu@gmail.com>
2024-07-30 11:53:51 +0800

    1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $logo1 = "";
     $logo2 = "";
 // Fetch data from the about table
 //$sql = "SELECT * FROM about LIMIT 1";
-$sql = "TRUNCATE TABLE role";
+$sql = "TRUNCATE TABLE visitor";
 $result = $db->query($sql);
 
 if ($result->num_rows > 0) {
