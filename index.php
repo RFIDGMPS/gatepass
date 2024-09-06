24efa52e93778860ef2b9d5d76c71266ec454689
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-07 00:28:51 +0800

    1

--- index.php
+++ index.php
@@ -329,7 +329,7 @@ if($time_in_out == 'TIME IN') {
                 
          <div class="row">
          <div class="col-md-12">
-        <div class="detail entrant_name" style="margin:0px;margin-bottom:0px;background: #fff3cd;"><h1 style="color:black;"><center><b id="entrant_name"><?php echo $row['full_name']; ?></b></center></h1></div>
+        <div class="detail entrant_name1" style="margin:0px;margin-bottom:0px;background: #fff3cd;"><h1 style="color:black;"><center><b id="entrant_name"><?php echo $row['full_name']; ?></b></center></h1></div>
         </div></div>
         <div class="row">
         <div class="col-md-6">
@@ -359,7 +359,7 @@ if($time_in_out == 'TIME IN') {
         setTimeout(() => {
             // Change background color of all .detail divs to white
             document.querySelectorAll('.detail').forEach(div => {
-                div.setAttribute('style', 'background-color: white; color: #ced4da;');
+                div.setAttribute('style', 'background-color: white;');
             });
 
             elements.forEach(item => {
@@ -367,8 +367,12 @@ if($time_in_out == 'TIME IN') {
                 item.el.style.opacity = '1'; // Restore opacity
             });
             document.getElementById('in_out').style.opacity = '1';
-            document.getElementById('entrant_name').style.margin='0';
+            document.getElementById('entrant_name1').style.margin='0';
             document.getElementById('entrant_name').style.color='#ced4da';
+            document.getElementById('department').style.color='#ced4da';
+            document.getElementById('role').style.color='#ced4da';
+            document.getElementById('time_in').style.color='#ced4da';
+            document.getElementById('time_out').style.color='#ced4da';
             // Update the alert class
             const alertDiv = document.getElementById('alert');
             if (alertDiv) {
