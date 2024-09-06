2c06aa613d50fb9e2f8bf6b57e3c51eb6f6b53f5
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-06 14:52:09 +0800

    1

--- index.php
+++ index.php
@@ -308,7 +308,10 @@ if (isset($_POST['submit'])) {
     
         // Fetch and display the results
         while ($row = mysqli_fetch_array($results)) { ?>
+         <div class="row">
+         <div class="col-md-9">
         <div class="detail entrant_name"><h1><?php echo $row['full_name']; ?></h1></div>
+        </div></div>
         <div class="row">
         <div class="col-md-5">
         <div class="detail deprt"><h1><?php echo $row['department']; ?></h1></div>
