dea32012f28b8d8e3396094e8baabcdc6bb50f9e
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-06 17:08:30 +0800

    1

--- index.php
+++ index.php
@@ -322,25 +322,21 @@ if($time_in_out == 'TIME IN') {
     
         // Fetch and display the results
         while ($row = mysqli_fetch_array($results)) { ?>
-        <style>
-    .fade {
-        transition: opacity 0.5s ease-in-out; /* Adjust the timing as needed */
-    }
-</style>
+        
          <img class="pic" src="admin/uploads/<?php echo $row['photo']; ?>" width="50px" height="50px" hidden>
                 
          <div class="row">
          <div class="col-md-12">
-        <div class="detail entrant_name fade" style="margin-top:0px;margin-bottom:0px;"><h1><center><b id="entrant_name"><?php echo $row['full_name']; ?></b></center></h1></div>
+        <div class="detail entrant_name" style="margin-top:0px;margin-bottom:0px;"><h1><center><b id="entrant_name"><?php echo $row['full_name']; ?></b></center></h1></div>
         </div></div>
         <div class="row">
         <div class="col-md-6">
-        <div class="detail deprt fade" ><h1 id="department"><?php echo $row['department']; ?> </h1></div>
-        <div class="detail role fade" ><h1 id="role"><?php echo $row['role']; ?></h1> </div>
+        <div class="detail deprt" ><h1 id="department"><?php echo $row['department']; ?> </h1></div>
+        <div class="detail role" ><h1 id="role"><?php echo $row['role']; ?></h1> </div>
         </div>
         <div class="col-md-6">
-        <div class="detail time_in fade" ><h1 id="time_in"><?php echo $row['time_in_am']; ?> </h1></div>
-        <div class="detail time_out fade" ><h1 id="time_out"><?php echo $row['time_out_pm']; ?> </h1></div>
+        <div class="detail time_in" ><h1 id="time_in"><?php echo $row['time_in_am']; ?> </h1></div>
+        <div class="detail time_out" ><h1 id="time_out"><?php echo $row['time_out_pm']; ?> </h1></div>
         </div>
         </div>
         <script>
