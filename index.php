cf84cbcdce96c2dafb4a8f0de175460b3d85698a
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-10 16:40:17 +0800

    1

--- index.php
+++ index.php
@@ -248,16 +248,16 @@ $password1 = mysqli_real_escape_string($db, $password1);
         }
     }();
   </script>
-     <script type="text/javascript" src="../assets/custom/js/admin_login.js"> </script>
+     <script type="text/javascript" src="assets/custom/js/admin_login.js"> </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/chart/chart.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/tempusdominus/js/moment.min.js"></script>
-    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
-    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="admin/lib/chart/chart.min.js"></script>
+    <script src="admin/lib/easing/easing.min.js"></script>
+    <script src="admin/lib/waypoints/waypoints.min.js"></script>
+    <script src="admin/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="admin/lib/tempusdominus/js/moment.min.js"></script>
+    <script src="admin/lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="admin/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="admin/js/main.js"></script>
 </body>
 
 </html>
