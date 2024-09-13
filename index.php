8d4864bb02d9af02fd7a0cd077e55e413f12338f
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-13 23:00:40 +0800

    1

--- index.php
+++ index.php
@@ -86,10 +86,8 @@ $password1 = mysqli_real_escape_string($db, $password1);
 $found=0;
 $sql = "SELECT * FROM rooms"; 
 $result = $db->query($sql);
-echo '<script type="text/javascript">
-alert("'.$location.'");
-alert("'.$password1.'");
-</script>';
+echo $location;
+echo $password1;
 if ($location == "Gate" && $password1 == "gate123") {
     // Store the username in session to indicate successful login
     //$_SESSION['username'] = $username;
