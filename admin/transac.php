9d19d4f875c3ed37dbe34e8265a39d5cbd6b9622
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-14 00:03:22 +0800

    1

--- admin/transac.php
+++ admin/transac.php
@@ -97,6 +97,10 @@ switch ($_GET['action'])
                 $room = $_POST['room'];
                 $department = $_POST['department'];
                 $desc = $_POST['desc'];
+                echo '<script type="text/javascript">
+                alert("'.$desc.'");
+               
+        </script>';
                 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
                 $query = "INSERT INTO rooms (room, department, password, description)
                 VALUES ('$room', '$department','$password','$desc')";
