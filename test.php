4c157c79a4ca305b7fcc40e9c1344057c5a79380
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-19 12:50:10 +0800

    1

--- test.php
+++ test.php
@@ -12,27 +12,6 @@ $sql = "
         pl.date_logged
     FROM personell_logs pl
     JOIN personell p ON pl.personell_id = p.id
-    
-
-    UNION
-
-    SELECT 
-        vl.photo,
-        vl.department,
-        NULL AS role,
-        vl.name AS full_name,
-        vl.time_in,
-        vl.time_out,
-        vl.date_logged
-    FROM visitor_logs vl
-   
-
-    ORDER BY 
-        -- Use time_out if available, otherwise use time_in
-        CASE 
-            WHEN time_out IS NOT NULL THEN time_out 
-            ELSE time_in 
-        END DESC LIMIT 1
 ";
 
 // Execute the query and check for errors
