bf1acaf90aa9f83076a87c17aceef41e0bda81f0
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-19 12:48:36 +0800

    1

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 include 'connection.php';
 
 $sql = "
-   SELECT 
+    SELECT 
         p.photo,
         p.department,
         p.role,
@@ -13,6 +13,26 @@ $sql = "
     FROM personell_logs pl
     JOIN personell p ON pl.personell_id = p.id
     WHERE pl.date_logged = CURRENT_DATE()
+
+    UNION
+
+    SELECT 
+        vl.photo,
+        vl.department,
+        NULL AS role,
+        vl.name AS full_name,
+        vl.time_in,
+        vl.time_out,
+        vl.date_logged
+    FROM visitor_logs vl
+    WHERE vl.date_logged = CURRENT_DATE()
+
+    ORDER BY 
+        -- Use time_out if available, otherwise use time_in
+        CASE 
+            WHEN time_out IS NOT NULL THEN time_out 
+            ELSE time_in 
+        END DESC LIMIT 1
 ";
 
 // Execute the query and check for errors
@@ -39,8 +59,8 @@ if ($result && $result->num_rows > 0) {
     // Fetch each row from the result set
     while ($row = $result->fetch_assoc()) {
         echo "<tr>
-            <td><img src='" . htmlspecialchars($row['personnel_id']) . "' width='50' height='50'></td>
-            <td>" . htmlspecialchars($row['personnel_id']) . "</td>
+            <td><img src='" . htmlspecialchars($row['photo']) . "' width='50' height='50'></td>
+            <td>" . htmlspecialchars($row['department']) . "</td>
             <td>" . htmlspecialchars($row['role']) . "</td>
             <td>" . htmlspecialchars($row['full_name']) . "</td>
             <td>" . htmlspecialchars($row['time_in']) . "</td>
