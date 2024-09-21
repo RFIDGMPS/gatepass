c5431855fce42c4ffe35d67840d1cd80440e0cf9
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-21 09:50:26 +0800

    1

--- main.php
+++ main.php
@@ -237,11 +237,11 @@ if (isset($_POST['submit'])) {
         // Check if user is already logged today
 $query1 = "SELECT * FROM personell_logs WHERE personnel_id = '{$user['id']}' AND date_logged = '$date_logged' AND location = '$location'";
 $result1 = mysqli_query($db, $query1);
-
+echo $user['department'];
+    echo $department;
 // Loop through the result set
 while ($row = mysqli_fetch_array($result1)) {
-    echo $user['department'];
-    echo $department;
+    
     // Check if user's department matches the log department
     if ($user['department'] == $department) {
         echo 'pass2';
