c6a0f1715376234edfe71738f665edf723ed25a4
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-21 16:12:16 +0800

    1

--- main.php
+++ main.php
@@ -336,7 +336,27 @@ if ($result1->num_rows > 0) {
     mysqli_close($db);
 }
 ?>
+<script>
+  
+  // Get the PHP-generated text
+  const text = "<?php echo $voice; ?>";
 
+  // Function to convert text to speech
+  const textToSpeech = (text) => {
+      const synth = window.speechSynthesis;
+
+      if (!synth.speaking && text) {
+          const utterance = new SpeechSynthesisUtterance(text);
+          synth.speak(utterance);
+      }
+  };
+
+  // Trigger text-to-speech if there's submitted text
+  if (text) {
+      textToSpeech(text);
+  }
+
+</script>
 
 <div id="rfidDisplay"></div>
 <br/>
