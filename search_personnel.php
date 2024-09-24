9661df4ca96625512d2614be0c8b46ae2e97463e
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-24 11:04:37 +0800

    1

--- search_personnel.php
+++ search_personnel.php
@@ -1,13 +1,13 @@
 <?php
 include 'connection.php';
-include 'admin/header.php';
 
 // Get the search query from URL
 $q = isset($_GET['q']) ? $db->real_escape_string($_GET['q']) : '';
 
 // SQL query to search in the personell table
-$sql = "SELECT CONCAT(first_name, ' ', middle_name, ' ', last_name) AS full_name, department, photo FROM personell WHERE 
-    CONCAT(first_name, ' ', middle_name, ' ', last_name) LIKE '%$q%'";
+$sql = "SELECT CONCAT(first_name, ' ', middle_name, ' ', last_name) AS full_name, department, photo 
+        FROM personell 
+        WHERE CONCAT(first_name, ' ', middle_name, ' ', last_name) LIKE '%$q%'";
 
 $result = $db->query($sql);
 
@@ -21,7 +21,7 @@ if ($result->num_rows > 0) {
             </tr>";
     while($row = $result->fetch_assoc()) {
         echo "<tr>
-               <td><img src='admin/uploads/" . $row['photo'] . "' width='50' height='50'></td>
+               <td><img src='" . $row['photo'] . "' width='50' height='50'></td>
                 <td>" . $row['department'] . "</td>
                 <td>" . $row['full_name'] . "</td>
               </tr>";
