e91876c0528cd726c704ccb199f92d56fb37ae1c
RFIDGMPS <kyebejeanu@gmail.com>
2024-09-24 13:35:37 +0800

    1

--- search_personnel.php
+++ search_personnel.php
@@ -1,11 +1,10 @@
 <?php
 include 'connection.php';
 
-
 // Get the search query from URL
 $q = isset($_GET['q']) ? $db->real_escape_string($_GET['q']) : '';
 
-// SQL query to search in the personell table
+// SQL query to search in the personnel table
 $sql = "SELECT CONCAT(first_name, ' ', middle_name, ' ', last_name) AS full_name, id, department, photo 
         FROM personell 
         WHERE CONCAT(first_name, ' ', middle_name, ' ', last_name) LIKE '%$q%'";
@@ -15,19 +14,17 @@ $result = $db->query($sql);
 // Output results as a table
 if ($result->num_rows > 0) {
     echo "<form><div style='height:180px;' class='table-responsive'>
-                    <table class='table table-border' id='myTable'>
-            <tr>
-          
-                <th>Photo</th>
-                <th>Department</th>
-                <th>Name</th>
-            </tr>";
-    while($row = $result->fetch_assoc()) {
+            <table class='table table-border' id='myTable'>
+                <tr>
+                    <th>Photo</th>
+                    <th>Department</th>
+                    <th>Name</th>
+                </tr>";
+    while ($row = $result->fetch_assoc()) {
         echo "<tr>
-       
-               <td><img src='admin/uploads/" . $row['photo'] . "' width='50' height='50'></td>
-                <td>" . $row['department'] . "</td>
-                <td>" . $row['full_name'] . "</td>
+                <td><img src='admin/uploads/" . htmlspecialchars($row['photo']) . "' width='50' height='50'></td>
+                <td>" . htmlspecialchars($row['department']) . "</td>
+                <td>" . htmlspecialchars($row['full_name']) . "</td>
               </tr>";
     }
     echo "</table></div></form>";
@@ -37,6 +34,7 @@ if ($result->num_rows > 0) {
 
 $db->close();
 ?>
+
 <script>
     // Get the table element
     const table = document.getElementById("myTable");
@@ -47,7 +45,7 @@ $db->close();
         if (event.target.tagName === "TD") {
             // Get the row that contains the clicked cell
             const row = event.target.parentNode;
-            
+
             // Retrieve the data from the cells
             const photo = row.cells[0].getElementsByTagName('img')[0].src; // Assuming the first cell contains the photo
             const department = row.cells[1].innerText;
@@ -57,7 +55,7 @@ $db->close();
             console.log("Photo:", photo);
             console.log("Department:", department);
             console.log("Name:", name);
-            
+
             // Example of displaying the data in an alert
             alert(`Name: ${name}\nDepartment: ${department}\nPhoto: ${photo}`);
         }
