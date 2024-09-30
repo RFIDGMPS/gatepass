aa068affca643abe1ac1ed1e403703be40cd0e90
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-01 07:42:08 +0800

    1

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include 'connection.php';
 
 
 // Display the current time using MySQL's CURRENT_TIMESTAMP function
-$result = $db->query("SELECT CURRENT_TIMESTAMP as current_time");
+$result = $db->query("SELECT CURRENT_DATE() as current_time");
 $row = $result->fetch_assoc();
 echo "MySQL Current Time: " . $row['current_time'];
 
