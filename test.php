4a1d4b90b365d22fd708642fb7d58c11785878a8
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-01 07:41:28 +0800

    1

--- test.php
+++ test.php
@@ -1,41 +1,21 @@
 <?php
+
 include 'connection.php';
 
 // SQL query to select all records from personell_logs table
-$sql = "    SELECT NOW() AS currentDate
-
-";
-$result = $db->query($sql);
-
-if ($result->num_rows > 0) {
-    echo "<table border='1' cellpadding='10'>
-            <tr>
-                <th>ID</th>
-                <th>Personnel ID</th>
-                <th>Date Logged</th>
-                <th>Time In</th>
-                <th>Time Out</th>
-                <th>Location</th>
-            </tr>";
-    
-    // Output data of each row
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td>{$row['id']}</td>
-                <td>{$row['currentDate']}</td>
-                <td>{$row['date_logged']}</td>
-                <td>{$row['time_in']}</td>
-                <td>{$row['time_out']}</td>
-                <td>{$row['location']}</td>
-              </tr>";
-
-        
-    }
-    echo "</table>";
+// Set the session time zone to Asia/Manila
+$timeZoneQuery = "SET time_zone = '+08:00';"; // Alternatively, use 'Asia/Manila'
+if ($db->query($timeZoneQuery) === TRUE) {
+    echo "MySQL time zone set to Asia/Manila.";
 } else {
-    echo "0 results";
- 
+    echo "Error setting MySQL time zone: " . $db->error;
 }
+
+// Display the current time using MySQL's CURRENT_TIMESTAMP function
+$result = $db->query("SELECT CURRENT_TIMESTAMP as current_time");
+$row = $result->fetch_assoc();
+echo "MySQL Current Time: " . $row['current_time'];
+
 
 // Close the database connection
 $db->close();
