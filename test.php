75c25441dc67eaf6e21fc1ece1a739b14e9c3dad
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-01 07:41:49 +0800

    1

--- test.php
+++ test.php
@@ -2,14 +2,6 @@
 
 include 'connection.php';
 
-// SQL query to select all records from personell_logs table
-// Set the session time zone to Asia/Manila
-$timeZoneQuery = "SET time_zone = '+08:00';"; // Alternatively, use 'Asia/Manila'
-if ($db->query($timeZoneQuery) === TRUE) {
-    echo "MySQL time zone set to Asia/Manila.";
-} else {
-    echo "Error setting MySQL time zone: " . $db->error;
-}
 
 // Display the current time using MySQL's CURRENT_TIMESTAMP function
 $result = $db->query("SELECT CURRENT_TIMESTAMP as current_time");
