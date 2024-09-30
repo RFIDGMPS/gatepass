5a1473d4a5e97022cead93d1523ca1ae15613fc0
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-01 07:24:08 +0800

    1

--- test.php
+++ test.php
@@ -2,10 +2,13 @@
 include 'connection.php';
 
 // SQL query to select all records from personell_logs table
-$sql = "    SELECT CURRENT_DATE() AS currentDate
+$timeZoneQuery = "SET time_zone = 'Asia/Manila';";
+if ($db->query($timeZoneQuery) === TRUE) {
+    echo "Time zone set to Asia/Manila.";
+} else {
+    echo "Error setting time zone: " . $db->error;
+}
 
-";
-$result = $db->query($sql);
 
 if ($result->num_rows > 0) {
     echo "<table border='1' cellpadding='10'>
