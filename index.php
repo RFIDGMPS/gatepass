69994c787127f4474e486e314787aaae291ba1bc
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-01 06:35:48 +0800

    1

--- index.php
+++ index.php
@@ -102,6 +102,8 @@ if (isset($_POST['submit'])) {
         }
         
     
+    }else {
+        echo '<script>alert("You\'re not allowed to open this room.");</script>';
     }
 
     // If not security personnel, check for room login
