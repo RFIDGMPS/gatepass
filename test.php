801a22ee3cb248263b96b8baf999ab439206eb7c
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-01 08:05:39 +0800

    1

--- test.php
+++ test.php
@@ -5,22 +5,9 @@ ini_set('display_errors', 1);
 include 'connection.php';
 
 // Check the database connection
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
-}
+$db->query("GRANT SELECT ON mysql.time_zone_name TO 'u510162695_gatepassdb'@'127.0.0.1'");
+$db->query("FLUSH PRIVILEGES");
 
-// Run the query to get time zones
-$result = $db->query("SELECT * FROM mysql.time_zone_name");
-
-// Check if the query was successful
-if ($result) {
-    // Loop through the results and display them
-    while ($row = $result->fetch_assoc()) {
-        echo $row['Time_zone_name'] . "<br>";
-    }
-} else {
-    echo "Query failed: " . $db->error;
-}
 
 // Close the database connection
 $db->close();
