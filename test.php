5d676b9f14ee62d550b6bd2e76ca3af24443e921
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-02 16:33:39 +0800

    1

--- test.php
+++ test.php
@@ -3,23 +3,15 @@
 
 <script>
 document.getElementById('myButton').addEventListener('click', function() {
-  Swal.fire({
-    title: 'Are you sure?',
-    text: "You won't be able to revert this!",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonText: 'Yes, do it!',
-    cancelButtonText: 'No, cancel!'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        'Done!',
-        'Your action was successful.',
-        'success'
-      ).then(() => {
-        window.location.href = 'main.php';  // Redirect after success
-      });
-    }
-  });
+    Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Your request has been saved',
+    showConfirmButton: false,
+    timer: 1500
+}).then(() => {
+    // This will run after the alert disappears
+    window.location.href = 'main.php';
+});
 });
 </script>
