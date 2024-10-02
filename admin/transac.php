0c1508d0990a1d9293fffdc8ccbf819a1ce05e3d
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-02 16:35:11 +0800

    1

--- admin/transac.php
+++ admin/transac.php
@@ -154,19 +154,8 @@ switch ($_GET['action'])
                                 mysqli_query($db, $query) or die('Error in updating Database');
                                
                                 // Alert and redirect
-                                echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
-    echo "<script>
-        Swal.fire({
-    position: 'top-end',
-    icon: 'success',
-    title: 'Your request has been saved',
-    showConfirmButton: false,
-    timer: 1500
-}).then(() => {
-    // This will run after the alert disappears
-    window.location.href = '../main.php';
-});
-    </script>";
+                                echo "<script> alert('success');</script>";
+   
                                 }
                             
     break;
