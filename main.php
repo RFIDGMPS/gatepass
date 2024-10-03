cdd8206e7fbd81bb7ba0485303e466e0bcdfa8df
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-03 13:01:07 +0800

    v

--- main.php
+++ main.php
@@ -1122,7 +1122,7 @@ document.getElementById('submitButton').addEventListener('click', function (e) {
                 icon: 'success',
                 title: 'Your request has been sent',
                 showConfirmButton: false,
-                timer: 1500
+                timer: 3000
             }).then(() => {
                 window.location.href = 'main.php'; // Redirect after 1.5 seconds
             });
