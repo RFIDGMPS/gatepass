63ef9bf540a18dfd286cc7b8c6922d0d8726958b
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-03 11:10:09 +0800

    1

--- admin/transac.php
+++ admin/transac.php
@@ -128,45 +128,48 @@ switch ($_GET['action'])
 
                   case 'add_lost_card':
 
-
-                        $id = mysqli_real_escape_string($db, $_POST['id']);
-                        $data_uri = $_POST['capturedImage'];
-                    
-                        if (empty($data_uri)) {
-                            echo 'error: Captured image is missing.';
-                            exit();
-                        }
-                    
-                        $encodedData = str_replace(' ', '+', $data_uri);
-                        list($type, $encodedData) = explode(';', $encodedData);
-                        list(, $encodedData) = explode(',', $encodedData);
-                        $decodedData = base64_decode($encodedData);
-                    
-                        $imageName = uniqid() . '.jpeg';
-                        $filePath = 'uploads/' . $imageName;
-                        $date_requested = date('Y-m-d H:i:s');
-                    
-                        if (file_put_contents($filePath, $decodedData)) {
-                            $query = "INSERT INTO lostcard (personnel_id, date_requested, verification_photo, status) 
-                                      VALUES ('$id', '$date_requested', '$imageName', 0)";
+                        if (isset($_POST['send'])) {
+                                $id = mysqli_real_escape_string($db, $_POST['id']);
+                                $data_uri = $_POST['capturedImage'];
                             
-                            if (mysqli_query($db, $query)) {
-                                echo ' <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script></script>   Swal.fire({
-                      
-                            icon: "success",
-                            title: "Your work has been saved",
-                            showConfirmButton: false,
-                            timer: 1500
-                        })</script>';
-                               
-                            } else {
-                                echo 'error: ' . mysqli_error($db) . ' - Query: ' . $query;
+                                if (empty($data_uri)) {
+                                    echo 'error: Captured image is missing.';
+                                    exit();
+                                }
+                            
+                                $encodedData = str_replace(' ', '+', $data_uri);
+                                list($type, $encodedData) = explode(';', $encodedData);
+                                list(, $encodedData) = explode(',', $encodedData);
+                                $decodedData = base64_decode($encodedData);
+                            
+                                $imageName = uniqid() . '.jpeg';
+                                $filePath = 'uploads/' . $imageName;
+                                $date_requested = date('Y-m-d H:i:s');
+                            
+                                if (file_put_contents($filePath, $decodedData)) {
+                                    $query = "INSERT INTO lostcard (personnel_id, date_requested, verification_photo, status) 
+                                              VALUES ('$id', '$date_requested', '$imageName', 0)";
+                            
+                                    if (mysqli_query($db, $query)) {
+                                        // Display SweetAlert on success
+                                        echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
+                                        echo '<script>
+                                            Swal.fire({
+                                                icon: "success",
+                                                title: "Your work has been saved",
+                                                showConfirmButton: false,
+                                                timer: 1500
+                                            }).then(() => {
+                                                window.location.href = "main.php"; // Redirect after the alert
+                                            });
+                                        </script>';
+                                    } else {
+                                        echo 'error: ' . mysqli_error($db) . ' - Query: ' . $query;
+                                    }
+                                } else {
+                                    echo 'error: Failed to save the image.';
+                                }
                             }
-                        } else {
-                            echo 'error: Failed to save the image.';
-                        }
-                    
-                            
     break;
 }
 ?>
