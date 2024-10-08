69b0452005aa69fb2c8a017a32d802b0cdab5a5d
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-08 11:00:19 +0800

    b

--- index.php
+++ index.php
@@ -117,7 +117,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                     <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
-                        <form role="form" id="logform" method="POST">
+                        <form role="form" id="logform" method="POST"> 
+                            <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
                             <div id="myalert3" style="display:none;">
                                 <div class="alert alert-danger">
                                     <span id="alerttext"></span>
