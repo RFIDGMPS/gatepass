09bff1bfaf414ada532badb66a1d209ce7da5aa0
RFIDGMPS <kyebejeanu@gmail.com>
2024-10-09 21:56:57 +0800

    s

--- admin/personell.php
+++ admin/personell.php
@@ -441,7 +441,7 @@ while ($row = $result->fetch_assoc()) {
                   </div>
                </div>
             </form>
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript">
 $(document).ready(function() {
